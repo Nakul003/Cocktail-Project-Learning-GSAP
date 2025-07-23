f755373166056cb313b2093984816a4dcf47b2d7
Nakul003 <nakulchauhan2017@gmail.com>
2025-07-23 15:46:18 +0530

    Implemented hero-section with cool text animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
-import React from 'react'
-import { ScrollTrigger, SplitText } from 'gsap/all'
-import gsap from 'gsap'
+import React from 'react';
+import { ScrollTrigger, SplitText } from 'gsap/all';
+import gsap from 'gsap';
+import Navbar from "./components/Navbar.jsx";
+import Hero from './components/Hero.jsx';
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
   return (
-    <div className='flex-center h-screen'>
-      <h3 className='text-3xl text-indigo-300'>Hello Gsap</h3>
-    </div>
+    <main>
+      <Navbar />
+      <Hero />
+      <div className='h-screen bg-black'></div>
+    </main>
   )
 }
 
